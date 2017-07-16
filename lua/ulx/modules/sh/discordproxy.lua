8d09d6ed3134a77107737a985b9c825c7fdaa7e9
Matthew Cohen <matt57358@gmail.com>
2017-07-15 20:52:27 -0400

    Localization changes

--- lua/ulx/modules/sh/discordproxy.lua
+++ lua/ulx/modules/sh/discordproxy.lua
@@ -3,11 +3,11 @@ local servername = "My Server" -- Server name for messages.
 local webhookurl   = "https://discordapp.com/api/webhooks/.../..." -- Discord webhook url.
 
 -- Localization text, %s will be replaced with correct values.
-local txtNoStaff = "There are no staff online."
-local txtCooldown = "You are being ratelimited and your message was not sent to the staff team via Discord. Please wait %s seconds." -- Value: Seconds left.
-local txtError = "An error occured and your message was not sent to the staff team via Discord."
+local txtNoStaff = "Unfortunately are currently no staff members online."
+local txtCooldown = "You are sending admin messages too fast and your message was not sent! Please wait %s seconds before sending another." -- Value: Seconds left.
+local txtError = "Uh oh! Your message could not be delivered to our staff. If this keeps happening please contact an Admin"
 local txtSent = "Your message was sent to the staff team via Discord."
-local txtMsg = "**%s** `%s` has sent a message to admins on **%s**:\n%s" -- Values: Player Name, Player SteamID, Server Name, Message
+local txtMsg = "**%s** `%s` has sent the following message on **%s** while no staff were online:\n%s" -- Values: Player Name, Player SteamID, Server Name, Message
 
 
 hook.Add("ULibCommandCalled","asayhooker",function(ply,cmd,args) -- Hook to asay.
